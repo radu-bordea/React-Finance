f72aba5f3c1793b6860856131474001b2e54cfdf
radu-bordea <radu.bordea.dev@gmail.com>
2025-02-22 11:45:40 +0100

    setput client , navigartion routes, navbar and themes

---
+++ README.md
@@ -0,0 +1,8 @@
+# install dependencies
+- npm i react-redux @reduxjs/toolkit react-router-dom @mui/material @emotion/react @emotion/styled @mui/icons-material @mui/x-data-grid
+- npm i -D @types/react-dom
+- npm i -D eslint eslint-config-react-app
+- npm i -D @types/node
+
+
+
---
+++ client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [preact()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
